a0f392de67d8c6c0a5f99e3ff6ddb1567ad7de82
Dani Poveda <danielpoveda@outlook.es>
2023-04-12 13:52:09 +0200

    Update upgrade-pkgs.sh
    I have refactored the code to take into account more packages that can break SSH connection when upgrading packages while connected via WiFi. Now I exclude them from being upgraded instead of putting them at the end of the list.

--- upgrade-pkgs.sh
+++ upgrade-pkgs.sh
@@ -2,24 +2,33 @@
 echo "Updating lists of available packages..."
 opkg update -V0
 echo -e "Done.\n"
-pkgs=`opkg list-upgradable | cut -d " " -f 1`
-if [[ -z "$pkgs" ]]; then
-  echo -e "No packages are upgradable.\n"
+
+excluded_packages="dropbear hostapd-common iw netifd wireless-regdb wpad-basic-wolfssl"
+
+pkgs=$(opkg list-upgradable | awk '{print $1}')
+upgradable_pkgs=""
+excluded_pkgs=""
+for pkg in $pkgs; do
+    if ! echo "$excluded_packages" | grep -qw "$pkg"; then
+        upgradable_pkgs="$upgradable_pkgs $pkg"
+    else
+        excluded_pkgs="$excluded_pkgs $pkg"
+    fi
+done
+
+if [[ -z "$upgradable_pkgs" ]]; then
+  echo -e "No packages are upgradable.\n"; exit 0 
 else
-  if [[ -z `echo "$pkgs" | grep netifd` ]]; then
-    echo -e "The following packages are upgradable:\n"
-    echo $pkgs
-  else
-    echo -e "The following packages are upgradable, 'netifd' is put in last place to avoid any issues:\n"
-    pkgs=`echo -e "$(echo "$pkgs" | grep -v netifd)\nnetifd"`
-    echo $pkgs
+  if [[ -n "$excluded_pkgs" ]]; then
+    echo -e "The following packages are upgradable, but will be excluded: $excluded_pkgs\n"
   fi
+  echo -e "The following packages are upgradable:\n$upgradable_pkgs"
   echo " "
   while true; do
     read -p "Do you want to upgrade them? (y/N) " choice
     case $choice in
-      [Yy]* ) echo -e "\nStarting upgrade..."; echo "$pkgs" | xargs -n 1 opkg upgrade -V1; echo -e "\nFinished."; exit 0; break;;
-      * ) echo -e "\nFinished."; exit 1;;  
+      [Yy]* ) echo -e "\nStarting upgrade..."; echo "$upgradable_pkgs" | xargs -n 1 opkg upgrade -V1; echo -e "\nFinished."; exit 0; break;;
+      * ) echo -e "\nFinished."; exit 1;;
     esac
   done
 fi
